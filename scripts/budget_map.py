79e46b64bd32ba1ff0e93447ae448feef1ce3f08
titipata <my.titipat@gmail.com>
2020-09-05 21:33:31 +0700

    Correct budget map plots

--- scripts/budget_map.py
+++ scripts/budget_map.py
@@ -1,31 +1,44 @@
 import json
+
 # import os.path as op
 import pandas as pd
 import folium
 
 
 if __name__ == "__main__":
-    df_budget_district_agg = pd.read_csv('../data/budget_district_info.csv'
-                                        ).dropna(subset=['dname']
-                                                ).groupby('dname')['งบแผนงาน'].sum().reset_index().rename(columns={'dname':'เขต'})
+    budget_total_df = pd.read_csv("../data/districts_total_budget.csv")
+    df_thai_address_data = pd.read_csv("../data/thai_address_data.csv")
+    lat_long_df = (
+        df_thai_address_data.query("province == 'กรุงเทพมหานคร'")
+        .groupby("district")[["latitude", "longitude"]]
+        .mean()
+        .reset_index()
+    )
 
-    df_thai_address_data = pd.read_csv('../data/thai_address_data.csv')
-    budget_sum_df = df_thai_address_data.loc[df_thai_address_data.province=='กรุงเทพมหานคร'
-                                            ].rename(columns={'district':'เขต'}
-                                                    ).merge(df_budget_district_agg, on='เขต', how='left'
-                                                           ).groupby('เขต')[['latitude','longitude','งบแผนงาน']].mean().dropna().reset_index()
-    
-    folium_map = folium.Map(location=[13.738, 100.597],
-                            zoom_start=11,
-                            tiles="Stamen Toner")
+    lat_long_df["district"] = lat_long_df.district.map(
+        lambda x: x.replace("ราษฎร์บูรณะ", "ราษฎร์บูรณะ").strip()
+    )
+    budget_total_df["dname"] = budget_total_df.dname.map(lambda x: x.strip())
+    budget_total_df = (
+        budget_total_df[["dname", "งบแผนงาน"]]
+        .rename(columns={"dname": "district"})
+        .merge(lat_long_df)
+    )
 
-    for _, row in budget_sum_df.iterrows():
-        if row['latitude'] != "":
-            folium.CircleMarker(location=(row["latitude"],
-                                        row["longitude"]),
-                                radius=row['งบแผนงาน'] / 50000000,
-                                color='#DF420D',
-                                popup='📍เขต: {} งบประมาณ: {} ล้านบาท'.format(row["เขต"], int(row['งบแผนงาน'] / 1000000)),
-                                fill=True).add_to(folium_map)
+    folium_map = folium.Map(
+        location=[13.738, 100.597], zoom_start=11, tiles="Stamen Toner"
+    )
 
-    folium_map.save("budget_map.html")
+    for _, row in budget_total_df.iterrows():
+        if row["latitude"] != "":
+            folium.CircleMarker(
+                location=(row["latitude"], row["longitude"]),
+                radius=row["งบแผนงาน"] / 30000000,
+                color="#DF420D",
+                popup="📍เขต: {} งบประมาณ: {} ล้านบาท".format(
+                    row["district"], int(row["งบแผนงาน"] / 1000000)
+                ),
+                fill=True,
+            ).add_to(folium_map)
+
+    folium_map.save("../plots/budget_map.html")
